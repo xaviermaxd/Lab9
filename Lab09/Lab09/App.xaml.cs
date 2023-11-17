ddf71a6fd3fd1dd04bb70709df1157f9e8528675
Alumno <Alumno@LAB1509-17>
2023-11-17 17:19:48 -0500

    Agregar archivos de proyecto.

---
+++ Lab09/Lab09/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab09
+{
+    public partial class App : Application
+    {
+
+        public static float ScreenHeight { get; set; }
+
+        public static float ScreenWidth { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            //DependencyService.Register<MockDataStore>();
+            //MainPage = new MainPage();
+            //MainPage = new PinchDemo();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
